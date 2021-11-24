9a8d4ea479114f3ccf0cc82ca639c609d42b07e4
Abhishek Tripathi <72148523+Abhishek-72@users.noreply.github.com>
2021-11-25 02:51:42 +0530

    Add files via upload

---
+++ day 1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+	int i;
+	long l;
+	char ch;
+	float f;
+	double d;
+	scanf("%d %ld %c %f %lf",&i,&l,&ch,&f,&d);
+	printf("%d\n%ld\n%c\n%0.3f\n%0.9lf\n",i,l,ch,f,d);
+	return 0;
+	}
+
+
+
+
+
+
+
+
+
